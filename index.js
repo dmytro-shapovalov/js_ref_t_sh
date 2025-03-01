777c70e41f91447a998830ac8e5040fa3739c866
dmytro-shapovalov <dmitry.shapovaloff@gmail.com>
2025-03-02 00:43:11 +0200

    feat: add original solution

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { runOop } from "./oop.js";
-import { runProc } from "./proc.js";
+import { runProc } from "./refactor_proc.js";
 import { data } from "./data.js";
+import { runOrig } from "./orig.js";
+
+runOrig(data);
+
+console.log("---");
 
 runOop(data);
 
